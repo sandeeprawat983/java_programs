f9473df1f0381f014d068e47ed080f34bda23180
sandeeprawat983 <rawat683sandeep@gmail.com>
2025-04-10 20:04:11 +0530

    slicing a stream with truncating, skipping the elements

--- src/streams/ex1/MainJava8_7.java
+++ src/streams/ex1/MainJava8_7.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Slicing a stream
+// Slicing a stream - part 1
 public class MainJava8_7 {
 
 	public static void main(String[] args) {
@@ -24,13 +24,23 @@ public class MainJava8_7 {
 		
 		System.out.println(slicedMenu1);
 		
+		// using dropWhile
 		List<Dish> slicedMenu2 = 
 				specialMenu.stream()
 							.dropWhile(dish -> dish.getCalories() < 320)
 							.collect(Collectors.toList());
 		
 		System.out.println(slicedMenu2);
-											
+		
+		// Truncating the stream
+		// create a list by selecting the first three dishes that have more than 
+		List<Dish> dishes = 
+					specialMenu.stream()
+								.filter(dish -> dish.getCalories() > 300)
+								.limit(3)
+								.collect(Collectors.toList());
+		
+		System.out.println(dishes);
 	}
 	
 }
