bd13f0a1a6acf7e8a020972dd711b599341bd85a
sandeeprawat983 <rawat683sandeep@gmail.com>
2025-04-17 16:33:33 +0530

    added another example of mapping with flatMap() methods

--- src/streams/ex1/MainJava8_9.java
+++ src/streams/ex1/MainJava8_9.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 // Mapping with map() and flatMap() methods - Part 1
 // applying a function to each element of a stream
 public class MainJava8_9 {
-	
+
 	public static void main(String[] args) {
 		List<Dish> menu = Arrays.asList(
                 new Dish("pork", 800, false, Type.MEAT),
@@ -20,42 +20,54 @@ public class MainJava8_9 {
                 new Dish("prawns", 300, false, Type.FISH),
                 new Dish("salmon", 450, false, Type.FISH)
         );
-		
+
 		/**
 		 	following code you pass a
 			method reference Dish::getName to the map method to extract the names of the dishes
 			in the stream
 		 */
-		List<String> dishNames = 
+		List<String> dishNames =
 				menu.stream()
 					.map(Dish::getName)
 					.collect(Collectors.toList());
-		
+
 		System.out.println(dishNames);
-		
+
 		/**
 		 	Given
 			a list of words, you’d like to return a list of the number of characters for each word
 		 */
 		List<String> words = Arrays.asList("Modern", "Java", "In", "Ation");
-		List<Integer> wordLengths = 
+		List<Integer> wordLengths =
 				words.stream()
 					 .map(String::length)
 					 .collect(Collectors.toList());
-		
+
 		System.out.println(wordLengths);
-		
+
 		/**
 		 	First extract the name and then
 		 	get the length of each element from a stream
 		 */
-		List<Integer> dishNameLengths = 
+		List<Integer> dishNameLengths =
 				menu.stream()
 					.map(Dish::getName)
 					.map(String::length)
 					.collect(Collectors.toList());
-		
+
 		System.out.println(dishNameLengths);
 	}
+
+        // Flattening streams
+        List<String> words = Arrays.asList("Hello", "World");
+        List<String> uniqueCharacters =
+                words.stream()
+                        .map(word -> word.split(""))
+                        .flatMap(Arrays::stream)
+                        .distinct()
+                        .toList();
+
+        System.out.println(uniqueCharacters);
+    }
 
 }
